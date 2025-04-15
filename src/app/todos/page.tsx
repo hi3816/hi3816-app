679e0bf9df3475f850b6de8b02493006d1e204f0
hi3816 <99782819+hi3816@users.noreply.github.com>
2025-04-15 15:22:45 +0900

    feat : add enter key suport for adding todos and prepare delete mutaiton
    - 엔터 키 입력 시 할 일 기능 추가
    - useDeleteTodo 훅 추가 및 deleteTodo 함수 등록

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -3,11 +3,13 @@
 import { useState } from 'react'
 import { useTodos } from '@/hooks/useTodos' // 할 일 조회 (useQuery)
 import { useAddTodo } from '@/hooks/useAddTodo' // 할 일 추가 (useMutation)
+import { useDeleteTodo } from '@/hooks/useDeleteTodo'
 
 //Unity에서 MonoBehaviour의 Start() + Update() 느낌. 페이지 자체를 구성하는 컴포넌트
 export default function TodoListPage(){
     const {data : todos, isLoading, isError } = useTodos()
     const { mutate: addTodo } = useAddTodo() // useAddTodo훅으로 부터 mutation 실행 함수 받아옴
+    const { mutate: deleteTodo } = useDeleteTodo();
     const [ input, setInput ] = useState('') //입력 필드의 상태 관리 (C#의 private string title 변수 느낌)
 
     const handleAdd = () => {
@@ -24,14 +26,19 @@ export default function TodoListPage(){
               {/* 할 일 입력창과 추가 버튼 */}
             <div className="flex gap-2 mb-4">
                 <input
-                value={input}
-                onChange={(e) => setInput(e.target.value)} // 입력값이 바뀔 때 상태 업데이트
-                className="border p-2 w-full rounded"
-                placeholder="할 일을 입력하세요"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)} // 입력값이 바뀔 때 상태 업데이트
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter'){
+                            handleAdd(); //엔터 누름 추가 버튼과 동일한 동작
+                        }
+                    }}
+                    className="border p-2 w-full rounded"
+                    placeholder="할 일을 입력하세요"
                 />
                 <button
-                onClick={handleAdd} // 추가 버튼 클릭 시 handleAdd 실행
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                    onClick={handleAdd} // 추가 버튼 클릭 시 handleAdd 실행
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
                 >
                 추가
                 </button>
@@ -40,9 +47,17 @@ export default function TodoListPage(){
             {/* todos 목록 출력 */}
             <ul className="space-y-2">
                 {todos?.map((todo) => (
-                <li key={todo.id} className="p-2 border rounded">
-                    <input type="checkbox" checked={todo.completed} readOnly className="mr-2" />
-                    {todo.title}
+                <li key={todo.id} className="min-h-14 p-2 border rounded flex justify-between items-center">
+                    <span>
+                        <input type="checkbox" checked={todo.completed} readOnly className="mr-2" />
+                        {todo.title}
+                    </span>
+                    <button
+                            onClick={()=>deleteTodo(todo.id)}
+                            className="text0red-500 hover:text-red-700 text-sm"
+                            >
+                                ❌
+                    </button>
                 </li>
                 ))}
             </ul>
